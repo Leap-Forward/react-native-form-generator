e0e4e1052816ed4452cf73d0a0695a07e60863b6
yernar <amergalyyernar@gmail.com>
2020-12-25 18:05:00 +0600

    fixed android styles

--- src/fields/PickerField.android.js
+++ src/fields/PickerField.android.js
@@ -2,17 +2,17 @@
 
 import React from 'react';
 import ReactNative from 'react-native';
-let { View, StyleSheet, TextInput, Text } = ReactNative;
+import { PickerComponent } from '../lib/PickerComponent';
 
+let { StyleSheet } = ReactNative;
 
-import {PickerComponent} from '../lib/PickerComponent';
-
-export class PickerField extends React.Component{
-  setValue(value){
+export class PickerField extends React.Component {
+  setValue(value) {
     this.refs.fieldComponent.setValue(value)
   }
-  render(){
-    return(<PickerComponent
+
+  render() {
+    return (<PickerComponent
       {...this.props}
       ref='fieldComponent'
       pickerProps={{
@@ -22,90 +22,84 @@ export class PickerField extends React.Component{
             this.props.containerStyle,
             this.props.pickerStyle,
           ],
-
-
       }}
-      />)
-    }
-
+    />)
   }
+}
 
 
+let formStyles = StyleSheet.create({
+  fieldContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#C8C7CC',
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    height: 45
+  },
+  form: {},
+  alignRight: {
+    marginTop: 7, position: 'absolute', right: 10
+  },
+  noBorder: {
+    borderTopWidth: 0,
+    borderBottomWidth: 0
+  },
+  separatorContainer: {
+    // borderTopColor: '#C8C7CC',
+    // borderTopWidth: 1,
+    paddingTop: 35,
+    borderBottomColor: '#C8C7CC',
+    borderBottomWidth: 1,
 
-      let formStyles = StyleSheet.create({
-        fieldContainer:{
-          borderBottomWidth: 1,
-          borderBottomColor: '#C8C7CC',
-          backgroundColor: 'white',
-          justifyContent: 'center',
-          height: 45
-        },
-        form:{
+  },
+  separator: {
 
-        },
-        alignRight:{
-           marginTop: 7, position:'absolute', right: 10
-        },
-        noBorder:{
-          borderTopWidth: 0,
-          borderBottomWidth: 0
-        },
-        separatorContainer:{
-          // borderTopColor: '#C8C7CC',
-          // borderTopWidth: 1,
-          paddingTop: 35,
-          borderBottomColor: '#C8C7CC',
-          borderBottomWidth: 1,
+    paddingLeft: 10,
+    paddingRight: 10,
+    color: '#6D6D72',
+    paddingBottom: 7
 
-        },
-        separator:{
+  },
+  fieldsWrapper: {
+    // borderTopColor: '#afafaf',
+    // borderTopWidth: 1,
+  },
+  horizontalContainer: {
+    flexDirection: 'row',
 
-          paddingLeft: 10,
-          paddingRight: 10,
-          color: '#6D6D72',
-          paddingBottom: 7
+    justifyContent: 'flex-start'
+  },
 
-        },
-        fieldsWrapper:{
-          // borderTopColor: '#afafaf',
-          // borderTopWidth: 1,
-        },
-        horizontalContainer:{
-          flexDirection: 'row',
+  fieldValue: {
+    fontSize: 34 / 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    marginRight: 10,
+    paddingTop: 4,
+    justifyContent: 'center',
 
-          justifyContent: 'flex-start'
-        },
+    color: '#C7C7CC'
+  },
+  fieldText: {
+    fontSize: 34 / 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    justifyContent: 'center',
+    lineHeight: 32
+  },
+  input: {
+    paddingLeft: 10,
+    paddingRight: 10,
 
-        fieldValue:{
-          fontSize: 34/2,
-          paddingLeft: 10,
-          paddingRight: 10,
-          marginRight:10,
-          paddingTop: 4,
-          justifyContent: 'center',
+  },
+  helpTextContainer: {
+    marginTop: 9,
+    marginBottom: 25,
+    paddingLeft: 20,
+    paddingRight: 20,
 
-          color: '#C7C7CC'
-        },
-        fieldText:{
-          fontSize: 34/2,
-          paddingLeft: 10,
-          paddingRight: 10,
-          justifyContent: 'center',
-          lineHeight: 32
-        },
-        input:{
-          paddingLeft: 10,
-          paddingRight: 10,
-
-        },
-        helpTextContainer:{
-          marginTop:9,
-          marginBottom: 25,
-          paddingLeft: 20,
-          paddingRight: 20,
-
-        },
-        helpText:{
-          color: '#7a7a7a'
-        }
-      });
+  },
+  helpText: {
+    color: '#7a7a7a'
+  }
+});
--- src/lib/PickerComponent.android.js
+++ src/lib/PickerComponent.android.js
@@ -15,7 +15,6 @@ export class PickerComponent extends React.Component {
     this.state = {
       value: props.value || props.label,
     }
-    this.pickerMeasures = {};
   }
 
   setValue(value) {
@@ -25,10 +24,7 @@ export class PickerComponent extends React.Component {
   }
 
   handleLayoutChange(e) {
-    let { x, y, width, height } = { ...e.nativeEvent.layout };
-
     this.setState(e.nativeEvent.layout);
-    //e.nativeEvent.layout: {x, y, width, height}}}.
   }
 
   handleValueChange(value) {
@@ -39,26 +35,6 @@ export class PickerComponent extends React.Component {
     if (this.props.onValueChange) this.props.onValueChange(value);
   }
 
-  _scrollToInput(event) {
-
-    if (this.props.onFocus) {
-      let handle = ReactNative.findNodeHandle(this.refs.inputBox);
-
-      this.props.onFocus(
-          event,
-          handle
-      )
-    }
-
-//      this.refs.picker.measure(this.getPickerLayout.bind(this));
-
-  }
-
-  _togglePicker(event) {
-    //this.setState({isPickerVisible:!this.state.isPickerVisible});
-    //this._scrollToInput(event);
-  }
-
   render() {
 
     return (<View><Field
@@ -66,7 +42,7 @@ export class PickerComponent extends React.Component {
             ref='inputBox'
             onPress={this.props.onPress}
         >
-          <View style={[this.props.containerStyle]}
+          <View style={[this.props.containerStyle,  { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' } ]}
                 onLayout={this.handleLayoutChange.bind(this)}>
             <Text style={this.props.labelStyle}>{this.props.label}</Text>
             <Picker ref='picker'
@@ -89,81 +65,4 @@ export class PickerComponent extends React.Component {
         </View>
     )
   }
-
 }
-
-
-let formStyles = StyleSheet.create({
-  form: {},
-  alignRight: {
-    marginTop: 7, position: 'absolute', right: 10
-  },
-  noBorder: {
-    borderTopWidth: 0,
-    borderBottomWidth: 0
-  },
-  separatorContainer: {
-    // borderTopColor: '#C8C7CC',
-    // borderTopWidth: 1,
-    paddingTop: 35,
-    borderBottomColor: '#C8C7CC',
-    borderBottomWidth: 1,
-
-  },
-  separator: {
-
-    paddingLeft: 10,
-    paddingRight: 10,
-    color: '#6D6D72',
-    paddingBottom: 7
-
-  },
-  fieldsWrapper: {
-    // borderTopColor: '#afafaf',
-    // borderTopWidth: 1,
-  },
-  horizontalContainer: {
-    flexDirection: 'row',
-
-    justifyContent: 'flex-start'
-  },
-  fieldContainer: {
-    borderBottomWidth: 1,
-    borderBottomColor: '#C8C7CC',
-    backgroundColor: 'white',
-    justifyContent: 'center',
-    height: 45
-  },
-  fieldValue: {
-    fontSize: 34 / 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    marginRight: 10,
-    paddingTop: 4,
-    justifyContent: 'center',
-
-    color: '#C7C7CC'
-  },
-  fieldText: {
-    fontSize: 34 / 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    justifyContent: 'center',
-    lineHeight: 32
-  },
-  input: {
-    paddingLeft: 10,
-    paddingRight: 10,
-
-  },
-  helpTextContainer: {
-    marginTop: 9,
-    marginBottom: 25,
-    paddingLeft: 20,
-    paddingRight: 20,
-
-  },
-  helpText: {
-    color: '#7a7a7a'
-  }
-});
